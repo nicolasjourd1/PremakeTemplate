0e8f656f7c63b12b8db5d6c1dc2868fdadfc181a
Nicolas Jourdain <nicolasjourd1@pm.me>
2024-06-22 19:50:30 +0200

    Mise à jour du template pour le développement de plusieurs projets

--- premake5.lua
+++ premake5.lua
@@ -1,18 +1,21 @@
 -- premake5.lua
 workspace "PremakeTemplate"
-    configurations { "Debug", "Release" }
-
-project "HelloWorld"
-    kind "ConsoleApp"
-    language "C++"
-    targetdir "bin/HelloWorld/%{cfg.buildcfg}"
-
-    files {"src/**.hpp", "src/**.cpp"}
+    architecture "x64"
+    configurations {"Debug", "Release"}
+    startproject "HelloWorld"
 
     filter "configurations:Debug"
-        defines { "DEBUG" }
-        symbols "On"
+       defines { "DEBUG" }
+       runtime "Debug"
+       symbols "On"
 
-    filter "configurations:Release"
-        defines { "RELEASE" }
-        optimize "On"
+   filter "configurations:Release"
+       defines { "RELEASE" }
+       runtime "Release"
+       optimize "On"
+       symbols "On"
+
+OutputDir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}"
+
+include "HelloLib/HelloLib.lua"
+include "HelloWorld/HelloWorld.lua"
